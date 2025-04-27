7b21b0af04d43d9ad1e0f475f2da9b7ad0799c02
dankru <noiafugace@gmail.com>
2025-04-27 19:34:45 +0700

    non flexible implementation

--- internal/cache/cache_test.go
+++ internal/cache/cache_test.go
@@ -1,7 +1,6 @@
 package cache
 
 import (
-	"context"
 	"github.com/dankru/Api_gateway_v2/internal/mocks"
 	"github.com/dankru/Api_gateway_v2/internal/models"
 	"github.com/google/uuid"
@@ -30,22 +29,19 @@ func TestCacheDecorator_CreateUser(t *testing.T) {
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			ctrl := gomock.NewController(t)
-			defer ctrl.Finish()
-
-			mockUserProvider := mocks.NewMockUserProvider(ctrl) // создаём мок
-			// Говорим что мы ожидаем вызов CreateUser с таким return
+			mockUserProvider := newMockUserProvider(t)
 			mockUserProvider.EXPECT().
 				CreateUser(gomock.Any(), tc.userRequest).
-				Return(uuid.New(), nil) // Возвращаем uuid
+				Return(uuid.New(), nil)
 
 			t.Logf("initializing cache decorator, ttl: %s\n", tc.cacheTTL)
 			cache := NewCacheDecorator(mockUserProvider, tc.cacheTTL)
 
 			t.Log("creating user through cache decorator")
-			id, err := cache.CreateUser(context.Background(), tc.userRequest)
+			id, err := cache.CreateUser(t.Context(), tc.userRequest)
+			t.Log("check err")
 			require.NoError(t, err)
-			// Проверяем, что uuid не пустой
+			t.Log("check uuid not nil")
 			require.NotEqual(t, uuid.Nil, id, "UUID не должен быть пустым (nil)\n")
 		})
 	}
@@ -66,30 +62,64 @@ func TestCacheDecorator_GetUser(t *testing.T) {
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			ctrl := gomock.NewController(t)
-			defer ctrl.Finish()
 
-			mockUserProvider := mocks.NewMockUserProvider(ctrl) // создаём мок
-			mockUserProvider.EXPECT().
-				GetUser(gomock.Any(), tc.ID.String()).
-				Return(&models.User{
-					ID:        tc.ID,
-					Name:      "Daniel",
-					Age:       30,
-					Anonymous: false,
-				}, nil) // Возвращаем юзера
+			cache, mockUserProvider := setupCacheAndMock(t, tc.cacheTTL)
 
-			t.Logf("initializing cache decorator, ttl: %s\n", tc.cacheTTL)
-			cache := NewCacheDecorator(mockUserProvider, tc.cacheTTL)
+			expectGetUserProvider(mockUserProvider, tc.ID)
 
-			t.Log("creating user through cache decorator\n")
-			user, err := cache.GetUser(context.Background(), tc.ID.String())
-			require.NoError(t, err)
+			user := getUserAndCheckNoError(t, cache, tc.ID)
 
-			t.Log("get user from cache\n")
-			cached, ok := cache.users[user.ID.String()]
-			require.Truef(t, ok, "пользователь должен быть в кэше \n")
-			require.Equal(t, tc.ID, cached.user.ID)
+			assertUserCached(t, cache, tc.ID)
+
+			cleanAndAssertUserNotCached(cache, t, tc.cacheTTL, user.ID)
 		})
 	}
+}
+
+func newMockUserProvider(t *testing.T) *mocks.MockUserProvider {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockUserProvider := mocks.NewMockUserProvider(ctrl)
+	return mockUserProvider
+}
+
+func setupCacheAndMock(t *testing.T, ttl time.Duration) (*CacheDecorator, *mocks.MockUserProvider) {
+	mockUserProvider := newMockUserProvider(t)
+	cache := NewCacheDecorator(mockUserProvider, ttl)
+
+	return cache, mockUserProvider
+}
+
+func expectGetUserProvider(mock *mocks.MockUserProvider, id uuid.UUID) {
+	mock.EXPECT().
+		GetUser(gomock.Any(), id.String()).
+		Return(&models.User{
+			ID:        id,
+			Name:      "Daniel",
+			Age:       30,
+			Anonymous: false,
+		}, nil)
+}
+
+func getUserAndCheckNoError(t *testing.T, cache *CacheDecorator, id uuid.UUID) *models.User {
+	user, err := cache.GetUser(t.Context(), id.String())
+	require.NoError(t, err)
+	return user
+}
+
+func assertUserCached(t *testing.T, cache *CacheDecorator, id uuid.UUID) {
+	cached, ok := cache.users[id.String()]
+	require.True(t, ok, "expected user ID %s to be cached", id)
+	require.Equal(t, id, cached.user.ID)
+}
+
+func cleanAndAssertUserNotCached(cache *CacheDecorator, t *testing.T, ttl time.Duration, id uuid.UUID) {
+	cache.StartCleaner(t.Context(), ttl)
+
+	time.Sleep(time.Second * 2)
+
+	t.Log("check user invalidated\n")
+	_, ok := cache.users[id.String()]
+	require.False(t, ok, "user must NOT be in cache \n")
 }
