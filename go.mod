7d6dca7288764f11268bde5d069e5043e74aec1f
dankru <noiafugace@gmail.com>
2025-04-27 13:44:41 +0700

    add user provider mock, cache tests

--- go.mod
+++ go.mod
@@ -14,16 +14,19 @@ require (
 	github.com/prometheus/client_golang v1.22.0
 	github.com/rs/zerolog v1.34.0
 	github.com/spf13/viper v1.20.1
+	github.com/stretchr/testify v1.10.0
 	go.opentelemetry.io/otel v1.35.0
 	go.opentelemetry.io/otel/exporters/jaeger v1.17.0
 	go.opentelemetry.io/otel/sdk v1.35.0
 	go.opentelemetry.io/otel/trace v1.35.0
+	go.uber.org/mock v0.5.1
 )
 
 require (
 	github.com/andybalholm/brotli v1.1.1 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/fsnotify/fsnotify v1.8.0 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.8 // indirect
 	github.com/go-logr/logr v1.4.2 // indirect
@@ -48,6 +51,7 @@ require (
 	github.com/mfridman/interpolate v0.0.2 // indirect
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.3 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/prometheus/client_model v0.6.1 // indirect
 	github.com/prometheus/common v0.62.0 // indirect
 	github.com/prometheus/procfs v0.16.0 // indirect
---
+++ internal/cache/cache_test.go
@@ -0,0 +1,96 @@
+package cache
+
+import (
+	"context"
+	"github.com/dankru/Api_gateway_v2/internal/mocks"
+	"github.com/dankru/Api_gateway_v2/internal/models"
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/require"
+	"go.uber.org/mock/gomock"
+	"testing"
+	"time"
+)
+
+func TestCacheDecorator_CreateUser(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	testCases := []struct {
+		name        string
+		cacheTTL    time.Duration
+		userRequest models.UserRequest
+	}{
+		{
+			name:     "валидный тест на создание юзера",
+			cacheTTL: time.Second,
+			userRequest: models.UserRequest{
+				Name:      "Дмитрий",
+				Age:       20,
+				Anonymous: false,
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+
+		t.Run(tc.name, func(t *testing.T) {
+			mockUserProvider := mocks.NewMockUserProvider(ctrl) // создаём мок
+			// Говорим что мы ожидаем вызов CreateUser с таким return
+			mockUserProvider.EXPECT().
+				CreateUser(gomock.Any(), tc.userRequest).
+				Return(uuid.New(), nil) // Возвращаем uuid
+
+			t.Logf("initializing cache decorator, ttl: %s\n", tc.cacheTTL)
+			cache := NewCacheDecorator(mockUserProvider, tc.cacheTTL)
+
+			t.Log("creating user through cache decorator")
+			id, err := cache.CreateUser(context.Background(), tc.userRequest)
+			require.NoError(t, err)
+			// Проверяем, что uuid не пустой
+			require.NotEqual(t, uuid.Nil, id, "UUID не должен быть пустым (nil)\n")
+		})
+	}
+}
+
+func TestCacheDecorator_GetUser(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	testCases := []struct {
+		name     string
+		cacheTTL time.Duration
+		ID       uuid.UUID
+	}{
+		{
+			name:     "валидный UUID тест",
+			cacheTTL: time.Second,
+			ID:       uuid.New(),
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			mockUserProvider := mocks.NewMockUserProvider(ctrl) // создаём мок
+			mockUserProvider.EXPECT().
+				GetUser(gomock.Any(), tc.ID.String()).
+				Return(&models.User{
+					ID:        tc.ID,
+					Name:      "Daniel",
+					Age:       30,
+					Anonymous: false,
+				}, nil) // Возвращаем юзера
+
+			t.Logf("initializing cache decorator, ttl: %s\n", tc.cacheTTL)
+			cache := NewCacheDecorator(mockUserProvider, tc.cacheTTL)
+
+			t.Log("creating user through cache decorator\n")
+			user, err := cache.GetUser(context.Background(), tc.ID.String())
+			require.NoError(t, err)
+
+			t.Log("get user from cache\n")
+			cached, ok := cache.users[user.ID.String()]
+			require.Truef(t, ok, "пользователь должен быть в кэше \n")
+			require.Equal(t, tc.ID, cached.user.ID)
+		})
+	}
+}
